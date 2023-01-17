78ef428268d801310c19e8dd0693253dc7c05322
wilson.vasquez <wilson.vasquez@plasticosuni.com>
2023-01-17 16:06:05 -0600

    Se Agrega iconscout y fonts de google por cdn

--- spotify/src/app/app.component.ts
+++ spotify/src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spotify';
+  title = 'spotify 2';
+
+
+  // car: CarModel =  {
+  //   brand: 'Ford',
+  //   model: 'Focus',
+  //   year: 2023
+  // }
+
+  // listCars : Array<CarModel>=[
+  //   {brand: 'Chevrolet',
+  //   model: 'Camaro',
+  //   year: 2023
+  // },
+  // {
+  //   brand: 'BMW',
+  //   model: 'ANG 2000',
+  //   year: 2023
+
+  // }
+  // ]
+
 }
+
+
+// interface CarModel {
+//   brand: String;
+//   model: String;
+//   year?: number;
+// }
+
