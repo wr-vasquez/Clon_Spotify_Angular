4daab42aa25cb7a09ae422b8040caf4f562c41c7
wilson.vasquez <wilson.vasquez@plasticosuni.com>
2023-01-24 16:29:48 -0600

    Creacion de Card Player

--- spotify/src/app/modules/tracks/pages/tracks-pages/tracks-pages.component.ts
+++ spotify/src/app/modules/tracks/pages/tracks-pages/tracks-pages.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../../data/tracks.json'
 
 @Component({
   selector: 'app-tracks-pages',
   templateUrl: './tracks-pages.component.html',
   styleUrls: ['./tracks-pages.component.css']
 })
-export class TracksPagesComponent {
+export class TracksPagesComponent implements OnInit {
+
+  mockTracksList: Array <TrackModel>= [
+  
+  ]
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+    const {data}: any =  (dataRaw as any).default
+   this.mockTracksList = data;
+    
+  }
 
 }
--- spotify/src/app/shared/components/section-generic/section-generic.component.ts
+++ spotify/src/app/shared/components/section-generic/section-generic.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-section-generic',
@@ -8,6 +9,11 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SectionGenericComponent implements OnInit  {
 
   @Input() title: string = ''
+  @Input() mode: 'small' | 'big' = 'big'
+  @Input() dataTracks: Array<TrackModel> = []
+
+  
+
   constructor() {}
 
   ngOnInit(): void {
